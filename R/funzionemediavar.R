c592e314d3afc156f8b08abbece1c3b1440abc86
annaerrichiello <60004370+annaerrichiello@users.noreply.github.com>
2020-02-02 15:03:36 +0100

    Add files via upload

---
+++ R/funzionemediavar.R
@@ -0,0 +1,5 @@
+myfunct<-function(x){
+  m <- mean(x)
+  v <- var(x)
+  print(c("MEAN" = m, "VARIANCE" = v))
+  }
---
+++ R/pca_leverage.R
@@ -0,0 +1,12 @@
+#'Calculate the leverage for a Principal Component Analysis
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+pca_leverage = function(x) {
+  res = prcomp(x)
+}
+
