ac8855fc9aab176003749dc336ca4a2a6c936ce7
annaerrichiello <60004370+annaerrichiello@users.noreply.github.com>
2020-02-05 18:14:14 +0100

    To Change
    To Change

--- R/app.R
+++ R/app.R
@@ -1,51 +1,56 @@
-anna_app <- dput(
-ui <- pageWithSidebar(
-  
-  # App title ----
-  headerPanel("Distribution Calculator by Anna Errichiello"),
-  
-  # Sidebar panel for inputs ----
-  sidebarPanel(
-    selectInput("Distribution","Please Select Distribution Type",
-                choices=c("Exponential","Normal")),
-    sliderInput("sampleSize","Please Select Sample Size:",
-                min=100, max=5000, value=1000, step=100),
-    conditionalPanel(condition = "input.Distribution == 'Normal",
-                     textInput("Mean","Please Select the mean", 10),
-                     textInput("sd","Please Select Standard Deviation")),
-    conditionalPanel(condition="input.Distribution == 'Exponential",
-                     textInput("lamda","Please Select Exponential Lamda:", 1))
-  ),
-  
-  # Main panel for displaying outputs ----
-  mainPanel(
-    plotOutput("plot")
+#'description
+#'
+#'@export
+appanna_app <- function() {
+
+  library(shiny)
+  ui <- pageWithSidebar(
+
+    # App title ----
+    headerPanel("Distribution Calculator by Anna Errichiello"),
+
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      selectInput("Distribution","Please Select Distribution Type",
+                  choices=c("Exponential","Normal")),
+      sliderInput("sampleSize","Please Select Sample Size:",
+                  min=100, max=5000, value=1000, step=100),
+      conditionalPanel(condition = "input.Distribution == 'Normal",
+                       textInput("Mean","Please Select the mean", 10),
+                       textInput("sd","Please Select Standard Deviation")),
+      conditionalPanel(condition="input.Distribution == 'Exponential",
+                       textInput("lamda","Please Select Exponential Lamda:", 1))
+    ),
+
+    # Main panel for displaying outputs ----
+    mainPanel(
+      plotOutput("plot")
+    )
   )
-)
-)
 
-anna_app <- dput(
-server <- function (input, output, session) {
-  
-  output$`plot` <- renderPlot({
-    
-    distType <- input$Distribution
-    size <- input$sampleSize
-    
-    if(distType == "Normal"){
-      
-      randomVec <- rnorm(size,mean = as.numeric(input$Mean), sd=as.numeric(input$sd))
-    }
-    else {
-      
-      randomVec <- rexp(size,rate = 1/as.numeric(input$lamda))
-    }
-    
-    hist(randomVec, col = "Pink")
-    
-    
-  })
+
+  server <- function (input, output, session) {
+
+    output$`plot` <- renderPlot({
+
+      distType <- input$Distribution
+      size <- input$sampleSize
+
+      if(distType == "Normal"){
+
+        randomVec <- rnorm(size,mean = as.numeric(input$Mean), sd=as.numeric(input$sd))
+      }
+      else {
+
+        randomVec <- rexp(size,rate = 1/as.numeric(input$lamda))
+      }
+
+      hist(randomVec, col = "Pink")
+
+
+    })
+  }
+
+  shinyApp(ui=ui, server=server)
+
 }
-)
-
-shinyApp(ui=ui, server=server)
---
+++ man/README.md
@@ -0,0 +1,7 @@
+Midterm Project in Coding for Data Science
+
+I created a shiny app that helps you to easily calculate the distribution of a sample, with a specific size, mean and you can choose between two types of distribution, Exponential and Normal. 
+In the case of Normal disribution, it is important to indicate even the standard deviation, otherwise an error message appears: "invalid number of 'breaks'".
+
+
+
