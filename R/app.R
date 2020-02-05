3d3362741f0c13974309b5fe8184e181feaf36de
annaerrichiello <60004370+annaerrichiello@users.noreply.github.com>
2020-02-05 17:22:42 +0100

    To Change
    To Change

--- R/app.R
+++ R/app.R
@@ -1,10 +1,9 @@
-library(shiny)
-
+anna_app <- dput(
 ui <- pageWithSidebar(
-
+  
   # App title ----
   headerPanel("Distribution Calculator by Anna Errichiello"),
-
+  
   # Sidebar panel for inputs ----
   sidebarPanel(
     selectInput("Distribution","Please Select Distribution Type",
@@ -17,37 +16,36 @@ ui <- pageWithSidebar(
     conditionalPanel(condition="input.Distribution == 'Exponential",
                      textInput("lamda","Please Select Exponential Lamda:", 1))
   ),
-
+  
   # Main panel for displaying outputs ----
   mainPanel(
     plotOutput("plot")
   )
 )
+)
 
-
+anna_app <- dput(
 server <- function (input, output, session) {
-
+  
   output$`plot` <- renderPlot({
-
+    
     distType <- input$Distribution
     size <- input$sampleSize
-
+    
     if(distType == "Normal"){
-
+      
       randomVec <- rnorm(size,mean = as.numeric(input$Mean), sd=as.numeric(input$sd))
     }
     else {
-
+      
       randomVec <- rexp(size,rate = 1/as.numeric(input$lamda))
     }
-
+    
     hist(randomVec, col = "Pink")
-
-
+    
+    
   })
 }
-
-
+)
 
 shinyApp(ui=ui, server=server)
-
