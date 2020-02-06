25a15afe0719513036fabde8c64815c28007044a
annaerrichiello <60004370+annaerrichiello@users.noreply.github.com>
2020-02-06 12:02:54 +0100

    To change
    To Change

--- R/app.R
+++ R/app.R
@@ -3,37 +3,38 @@
 #'@export
 anna_app <- function() {
 
-  library(shiny)
-  ui <- pageWithSidebar(
+  requireNamespace(shiny)
+  ui <- shiny::pageWithSidebar(
 
     # App title ----
-    headerPanel("Distribution Calculator by Anna Errichiello"),
+    shiny::headerPanel("Distribution Calculator by Anna Errichiello"),
 
     # Sidebar panel for inputs ----
-    sidebarPanel(
-      selectInput("Distribution","Please Select Distribution Type",
+    shiny::sidebarPanel(
+      shiny::selectInput("Distribution","Please Select Distribution Type",
                   choices=c("Exponential","Normal")),
-      sliderInput("sampleSize","Please Select Sample Size:",
+      shiny::sliderInput("sampleSize","Please Select Sample Size:",
                   min=100, max=5000, value=1000, step=100),
-      conditionalPanel(condition = "input.Distribution == 'Normal",
-                       textInput("Mean","Please Select the mean", 10),
-                       textInput("sd","Please Select Standard Deviation")),
-      conditionalPanel(condition="input.Distribution == 'Exponential",
-                       textInput("lamda","Please Select Exponential Lamda:", 1))
+      shiny::conditionalPanel(condition = "input.Distribution == 'Normal",
+                       shiny::textInput("Mean","Please Select the mean", 10),
+                       shiny::textInput("sd","Please Select Standard Deviation")),
+      shiny::conditionalPanel(condition="input.Distribution == 'Exponential",
+                       shiny::textInput("lamda","Please Select Exponential Lamda:", 1))
     ),
 
     # Main panel for displaying outputs ----
-    mainPanel(
-      plotOutput("plot")
+    shiny::mainPanel(
+      shiny::plotOutput("plot")
     )
   )
 
-
+  requireNamespace(shiny)
   server <- function (input, output, session) {
 
-    output$`plot` <- renderPlot({
+    output$`plot` <- shiny::renderPlot({
 
       distType <- input$Distribution
+
       size <- input$sampleSize
 
       if(distType == "Normal"){
@@ -51,6 +52,9 @@ anna_app <- function() {
     })
   }
 
-  shinyApp(ui=ui, server=server)
+  shiny::shinyApp(ui=ui, server=server)
 
 }
+
+if(getRversion() >= "0.1.0")  utils::globalVariables("shiny")
+
