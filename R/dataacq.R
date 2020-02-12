1a9dc3bbdefc740318d6dc9b499298ac0aa439ee
annaerrichiello <60004370+annaerrichiello@users.noreply.github.com>
2020-02-12 16:44:30 +0100

    To change
    To change

--- R/dataacq.R
+++ R/dataacq.R
@@ -1,8 +1,9 @@
-#' @export
-#'
-#'
-#'
-alcons <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/alcohol-consumption/drinks.csv", sep=",")
-head(alcons)
-save(alcons, file="C:/Users/Anna Errichiello/Desktop/progetto/be4dd3dd/ShinyApp/be4dd3dd/data/alcon.rda" )
+# @export
+#
+#
+#
+#alcons <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/alcohol-consumption/drinks.csv", sep=",")
+#head(alcons)
+#save(alcons, file="C:/Users/Anna Errichiello/Desktop/progetto/be4dd3dd/ShinyApp/be4dd3dd/data/alcons.rda" )
+
 
