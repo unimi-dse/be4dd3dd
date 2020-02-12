90d9f3293ca9af640b5fea4275d9121dda7318c3
annaerrichiello <60004370+annaerrichiello@users.noreply.github.com>
2020-02-12 16:51:59 +0100

    Testing
    Testing

--- R/dataacq.R
+++ R/dataacq.R
@@ -1,9 +1,24 @@
-# @export
-#
-#
-#
-#alcons <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/alcohol-consumption/drinks.csv", sep=",")
-#head(alcons)
-#save(alcons, file="C:/Users/Anna Errichiello/Desktop/progetto/be4dd3dd/ShinyApp/be4dd3dd/data/alcons.rda" )
+#' @title alcons function
+#'
+#' @description The "alcons" function is an easy way to visualize the dataset
+#'
+#' The function needs the argument "alcons".
+#' The output shows a graph built using the "ggplot2 package"
+#'
+#' @author Anna Errichiello <anna.errichiello@studenti.unimi.it>
+#'
+#' @export
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+
+alcons <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/alcohol-consumption/drinks.csv", sep=",")
+head(alcons)
+save(alcons, file="C:/Users/Anna Errichiello/Desktop/progetto/be4dd3dd/ShinyApp/be4dd3dd/data/alcons.rda" )
 
 
