d0d2591d818122e40444dba29e7427d0728171a2
annaerrichiello <60004370+annaerrichiello@users.noreply.github.com>
2020-02-12 17:15:33 +0100

    To complete
    To complete

--- R/data.R
+++ R/data.R
@@ -7,6 +7,9 @@
 #'
 #' @format data.frame with 5 variables and 193 observations
 #' \describe{
-#'   \item{age}{age}
-#'   \item{n}{n}
+#'   \item{country}{considered country}
+#'   \item{beer_servings}{average alcohol content and average serving size of beer}
+#'   \item{spirit_servings}{ average alcohol content and average serving size of spirit}
+#'   \item{wine_servings}{ average alcohol content and average serving size of wine}
+#'   \item{total_litres_of_pure_alcohol}{alcohol consumed}
 #' "alcons"
